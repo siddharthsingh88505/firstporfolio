eb898f2a7db79534de49efb3d7cccd0b02cd4993
Siddharth Singh <82952572+siddharthsingh88505@users.noreply.github.com>
2022-01-21 18:43:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
 let myText =
 ["Developer!","Designer!","Programmer!","Freelancer!"];
 let myE = document.querySelector('main>section:first-child div:last-child')
