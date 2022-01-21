f3401bb3074b0448da3a9f1c575bf44c7c32d3da
Siddharth Singh <82952572+siddharthsingh88505@users.noreply.github.com>
2022-01-21 19:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var viewMode = document.cookie.split("view-mode");
 if(viewMode == "desktop"){
     viewport.setAttribute('content', 'width=102');
+    console.log('pc')
 }else if (viewMode == "mobile"){
+    console.log('mobile')
     viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
 }
 
