c6bf186300a6203bd7cda678fcbc409c6e319a6e
Siddharth Singh <82952572+siddharthsingh88505@users.noreply.github.com>
2022-01-21 18:49:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var viewMode = getCookie("view-mode");
 if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'initial-scale=-0.3');
+    viewport.setAttribute('content', 'initial-scale=0.1');
 }else if (viewMode == "mobile"){
     viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0');
 }
